8ef1f64035b7d9d0831683c43a1184fb399732a5
unknown <eliasjrcosta886@gmail.com>
2022-10-10 19:04:21 -0300

    finalização do projeto

--- imc.js
+++ imc.js
@@ -2,15 +2,33 @@ let peso = document.getElementById('peso')
 let altura = document.getElementById('altura')
 let resp1 = document.querySelector('.txtres1')
 let resp2 = document.querySelector('.txtres2')
-let resp3 = document.querySelector('.txtres3')
+let contres = document.querySelector('.containerres')
+let butcalc = document.getElementById('submcalc')
+
+function limpar(){
+    peso.value = ''
+    altura.value = ''
+    resp1.innerHTML = ''
+    resp2.innerHTML = ''
+    butcalc.style.position = 'absolute'
+    contres.style.display = 'none'
+}
 
 function calcular(){
     let resultado = peso.value / (altura.value * altura.value)
-    resp1.style.display = 'block'
+    resultado.toFixed(2)
     resp1.innerHTML = `Seu IMC é: ${resultado.toFixed(2)}`
-    resp2.style.display = 'block'
-    
-    if(resultado < 18.5){
+    contres.style.display = 'block'
+    butcalc.style.position = 'relative'
+
+    if(peso.value == '' || altura.value == ''){
+        window.alert('Preencha todas as informações')
+        resp1.innerHTML = ''
+        resp2.style.display = 'none'
+        contres.style.display = 'none'
+        butcalc.style.position = 'absolute'
+
+    }else if(resultado < 18.5){
         resp2.innerHTML = 'Você está abaixo do peso !'
     }else if(resultado >= 18.5 && resultado <= 24.9){
         resp2.innerHTML = 'Você está no peso ideal !'
